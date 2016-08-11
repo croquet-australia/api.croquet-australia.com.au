dd4010802ecbeb3fb7864ee52e1d959934c34b6f
Tim Murphy <tim@26tp.com>
2016-08-11 11:16:59 +1000

    Add spreadhseet columns for M & W functions

--- source/CroquetAustralia.DownloadTournamentEntries/ReadModels/FunctionReadModel.cs
+++ source/CroquetAustralia.DownloadTournamentEntries/ReadModels/FunctionReadModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using CroquetAustralia.Domain.Features.TournamentEntry.Commands;
 
 namespace CroquetAustralia.DownloadTournamentEntries.ReadModels
@@ -8,9 +9,11 @@ namespace CroquetAustralia.DownloadTournamentEntries.ReadModels
     {
         public FunctionReadModel(SubmitEntry.LineItem lineItem)
         {
+            Id = lineItem.Id;
             Quantity = lineItem?.Quantity ?? 0;
         }
 
+        public Guid Id { get; private set; }
         public int Quantity { get; private set; }
     }
 }

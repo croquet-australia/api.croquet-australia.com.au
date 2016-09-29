46054bac6a54c61aa1b144f101cf243e7b7af070
Tim Murphy <tim@26tp.com>
2016-09-29 15:36:13 +1000

    Add email generator for 'Australian GC President's Eights - Expressions of Interest'

---
+++ source/CroquetAustralia.QueueProcessor/Email/EmailGenerators/GCEightsEOIEmailGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using CroquetAustralia.Domain.Features.TournamentEntry.Events;
+
+namespace CroquetAustralia.QueueProcessor.Email.EmailGenerators
+{
+    public class GCEightsEOIEmailGenerator : BaseEmailGenerator
+    {
+        /* todo: remove hard coding of email addresses */
+        private static readonly EmailAddress GCSC = new EmailAddress("acquinn@bigpond.com", "Croquet Australia - Secretary GC Selection");
+
+        private static readonly EmailAddress[] BCC =
+        {
+            new EmailAddress("admin@croquet-australia.com.au", "Croquet Australia"),
+            GCSC
+        };
+
+        public GCEightsEOIEmailGenerator(EmailMessageSettings emailMessageSettings)
+            : base(emailMessageSettings, GCSC, GetBCC(emailMessageSettings))
+        {
+        }
+
+        protected override string GetTemplateName(EntrySubmitted entrySubmitted)
+        {
+            return "GCEightsEOI";
+        }
+
+        private static EmailAddress[] GetBCC(EmailMessageSettings emailMessageSettings)
+        {
+            return emailMessageSettings.Bcc.Any() ? BCC : new EmailAddress[] {};
+        }
+    }
+}

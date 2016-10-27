e17fc6592550e3e390131d088229a1e718bbf320
Tim Murphy <tim@26tp.com>
2016-10-28 05:44:47 +1100

    Configure spreadsheet to include AC Open events

--- source/CroquetAustralia.DownloadTournamentEntries/Program.cs
+++ source/CroquetAustralia.DownloadTournamentEntries/Program.cs
@@ -15,8 +15,8 @@ namespace CroquetAustralia.DownloadTournamentEntries
             {
                 var tournamentIds = new[]
                 {
-                    Guid.Parse(TournamentsRepository.TournamentIdGcHandicapDoubles2016),
-                    Guid.Parse(TournamentsRepository.TournamentIdGcHandicapSingles2016)
+                    Guid.Parse(TournamentsRepository.TournamentIdAcOpenDoubles2016),
+                    Guid.Parse(TournamentsRepository.TournamentIdAcOpenSingles2016)
                 };
 
                 var tournamentsRepository = new TournamentsRepository();
